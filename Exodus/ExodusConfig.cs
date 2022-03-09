44f40f6f37c5bc1f5ee36851a0c663a7feca8e72
tryashtar <tryashtar@gmail.com>
2022-03-09 16:40:22 -0700

    Add project files.

---
+++ Exodus/ExodusConfig.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Win32;
+using System.Collections.ObjectModel;
+using YamlDotNet.Core;
+using YamlDotNet.Core.Events;
+using YamlDotNet.Serialization;
+
+namespace Exodus;
+
+public class ExodusConfig : IYamlConvertible
+{
+    public void Read(IParser parser, Type expectedType, ObjectDeserializer nestedObjectDeserializer)
+    {
+        parser.Consume<MappingStart>();
+        
+        parser.Consume<MappingEnd>();
+    }
+
+    public void Write(IEmitter emitter, ObjectSerializer nestedObjectSerializer)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Exodus/Program.cs
@@ -0,0 +1,6 @@
+﻿using Exodus;
+using Microsoft.Win32;
+
+var key = new NestedRegistry(Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"));
+var serializer = new YamlDotNet.Serialization.Serializer();
+serializer.Serialize(Console.Out, key);
